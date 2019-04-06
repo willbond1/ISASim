34f69146dba36db81389a049deadffe0c7225715
William Sullivan <willbond1@gmail.com>
2019-04-06 12:25:21 -0400

    Decode finished

--- Python/cpu.py
+++ Python/cpu.py
@@ -36,31 +36,74 @@ class CPU:
         next_inst = active_memory.read(self.registers[PC])
         return next_inst
     
-    # decode instruction in fetch_stage
+    # decode instruction in fetch_stage and return instruction fields as tuple
     def decode(self):
         cond_code = (self.fetch_stage & 0xF0000000) >> 28
-        inst_code = (self.fetch_stage & 0x0C000000) >> 26
+        inst_code = (self.fetch_stage & 0xC000000) >> 26
 
         def decode_ALU():
-            pass
+            I = (self.fetch_stage & 0x2000000) >> 25
+            opcode = (self.fetch_stage & 0x1E00000) >> 21
+            S = (self.fetch_stage & 0x100000) >> 20
+            rd = (self.fetch_stage & 0xF0000) >> 16
+            ro = (self.fetch_stage & 0xF000) >> 12
+
+            if I == 0:
+                shift = (self.fetch_stage & 0xF80) >> 7
+                shift_type = (self.fetch_stage & 0x60) >> 5
+                T = (self.fetch_stage & 0x10) >> 4
+                ro2 = (self.fetch_stage & 0xF)
+
+                return (cond_code, inst_code, I, opcode, S, rd, ro, shift, shift_type, T, ro2)
+            else:
+                rotate = (self.fetch_stage & 0xF00) >> 8
+                operand_immediate = (self.fetch_stage & 0xFF)
+
+                return (cond_code, inst_code, I, opcode, S, rd, ro, rotate, operand_immediate)
         
         def decode_memory():
-            pass
+            I = (self.fetch_stage & 0x2000000) >> 25
+            P = (self.fetch_stage & 0x1000000) >> 24
+            U = (self.fetch_stage & 0x800000) >> 23
+            W = (self.fetch_stage & 0x400000) >> 22
+            L = (self.fetch_stage & 0x200000) >> 21
+            rn = (self.fetch_stage & 0x1E0000) >> 17
+            rd = (self.fetch_stage & 0x1E000) >> 13
+
+            if I == 0:
+                immediate_offset = (self.fetch_stage & 0x1FFF)
+                return (cond_code, inst_code, I, P, U, W, L, rn, rd, immediate_offset)
+            else:
+                offset_shift = (self.fetch_stage & 0x1F00) >> 8
+                shift_type = (self.fetch_stage & 0xC0) >> 6
+                ro = (self.fetch_stage & 0xF)
+
+                return (cond_code, inst_code, I, P, U, W, L, rn, rd, offset_shift, shift_type, ro)
         
         def decode_control():
-            pass
+            I = (self.fetch_stage & 0x2000000) >> 25
+            if I == 0:
+                L = (self.fetch_stage & 0x800000) >> 23
+                ra = (self.fetch_stage & 0x1F)
+
+                return (cond_code, inst_code, I, L, ra)
+            else:
+                L = (self.fetch_stage & 0x1000000) >> 24
+                offset = (self.fetch_stage & 0xFFFFFF)
+
+                return (cond_code, inst_code, I, L, offset)
 
         if inst_code == 0: # ALU
-            pass
+            return decode_ALU()
         elif inst_code == 1: # Memory operation
-            pass
+            return decode_memory()
         elif inst_code == 2: # Control operation
-            pass
+            return decode_control()
         elif inst_code == 3: # No op
-            pass
+            return (empty_stage,)
         else:
             print('Error decoding instruction')
-            return None
+            return (None,)
 
     # execute instruction in decode_stage
     def execute(self):
