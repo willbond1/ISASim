7bb009471232fc6861133f28d968cb73e0120dad
William Sullivan <willbond1@gmail.com>
2019-04-29 09:19:29 -0400

    Memory fix

--- Python/cpu.py
+++ Python/cpu.py
@@ -431,11 +431,11 @@ class CPU:
 
                     if read_word: # read went through
                         self.memory_accessing = False
-                        self.forward_register[rd] = read_word
+                        self.forward_register[rd] = read_word.from_bytes(4, byteorder='big')
                         if W:
-                            return (read_word, write_addr)
+                            return (read_word.from_bytes(4, byteorder='big'), write_addr)
                         else:
-                            return (read_word, None)
+                            return (read_word.from_bytes(4, byteorder='big'), None)
                     else:
                         return None
                 else:
